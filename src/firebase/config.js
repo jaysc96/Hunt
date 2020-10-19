359a10cc151bcfd1963cf6d8f8b8e8ba73e25849
Jay Singh Chauhan <jjchauhan@outlook.com>
2020-10-19 19:18:08 +0530

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBMDxLloFU2dDmRoIYd8nLmmua5HaQmuj4",
+    authDomain: "hunt-317fe.firebaseapp.com",
+    databaseURL: "https://hunt-317fe.firebaseio.com",
+    projectId: "hunt-317fe",
+    storageBucket: "hunt-317fe.appspot.com",
+    messagingSenderId: "682638829284",
+    appId: "1:682638829284:web:2bfbacbb4950d25b356ef1"
+  };
+
+  export default firebaseConfig;
