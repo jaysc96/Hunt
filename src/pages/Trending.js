6395eec097b27ccb01cf52ca8ee5f88596a35e13
Jay Singh Chauhan <jjchauhan@outlook.com>
2020-11-04 17:27:58 +0530

    Product List

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { IonContent, IonPage } from "@ionic/react";
-import LargeHeader from "../components/Header/LargeHeader"
-import SmallHeader from "../components/Header/SmallHeader"
+import LargeHeader from "../components/Header/LargeHeader";
+import SmallHeader from "../components/Header/SmallHeader";
+import ProductList from "../components/Product/ProductList";
 
-const Trending = () => {
+const Trending = (props) => {
   return (
     <IonPage>
       <SmallHeader title="Trending" />
       <IonContent fullscreen>
           <LargeHeader title="Trending" />
+          <ProductList location={props.location} />
       </IonContent>
     </IonPage>
   );
